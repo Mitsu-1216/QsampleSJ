4795f4c47078a87ec75bf4fb5166a61ef4a2671d
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-06-17 22:33:25 +0900

    modify

--- sampleSJ/src/main/java/servlet/LogoutServlet.java
+++ sampleSJ/src/main/java/servlet/LogoutServlet.java
@@ -11,8 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 
-@WebServlet("/Logout")
-public class Logout extends HttpServlet {
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
