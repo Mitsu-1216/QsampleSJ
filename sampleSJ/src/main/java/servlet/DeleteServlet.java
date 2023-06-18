fa0d0853db794564e6573b45546f250bf1857745
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-06-18 19:24:49 +0900

    delete

--- sampleSJ/src/main/java/servlet/DeleteServlet.java
+++ sampleSJ/src/main/java/servlet/DeleteServlet.java
@@ -16,27 +16,24 @@ import model.TodoBeans;
 @WebServlet("/DeleteServlet")
 public class DeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		String text = request.getParameter("text");
-		
 
-			ServletContext application = this.getServletContext();
-			List<TodoBeans> todoList = (List<TodoBeans>) application.getAttribute("todoList");
+		ServletContext application = this.getServletContext();
+		List<TodoBeans> todoList = (List<TodoBeans>) application.getAttribute("todoList");
 
-//			HttpSession session = request.getSession();
-//			UserBeans loginUser = (UserBeans) session.getAttribute("user");
-//			String name = loginUser.getName();	
-			
-				
-			todoList.remove(2);
-			
-			
+		int listIndex = 0;
+		for (int i = 0; i < todoList.size(); i++) {
+			if (todoList.get(i).getText().equals(text))
+				listIndex = i;
+		}
 
+		todoList.remove(listIndex);
 
-			application.setAttribute("todoList", todoList);
-
+		application.setAttribute("todoList", todoList);
 
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/todo.jsp");
 		dispatcher.forward(request, response);
